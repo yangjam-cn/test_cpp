5d7e354a80efac1e4b0cb7dd72cd62b3f3b40645
cn_yangjam <1171267147@qq.com>
2023-04-09 10:33:30 +0800

    修改目录

--- vs_cpp/xmake.lua
+++
@@ -1,9 +0,0 @@
-
--- 添加第三方库，通过system=true指定优先本地搜索
-add_requires("gtest", {system=true, alias="gtest"})
-
-
-
-build_case('case037')
-build_case('test_spdlog', {'spdlog', 'fmt'})
-build_case('case038', nil, {'gtest'})
